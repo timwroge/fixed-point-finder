3a22f78f5f41971ea3356bc4019136ea181cb392
Timothy Wroge <tiw51@pitt.edu>
2020-03-20 16:45:18 -0400

    Add some fixed point visualizations

--- example/GridSearch.py
+++ example/GridSearch.py
@@ -1,4 +1,12 @@
 from Utils import print_status
+import sys
+PATH_TO_FIXED_POINT_FINDER = '../'
+sys.path.insert(0, PATH_TO_FIXED_POINT_FINDER)
+from FixedPointFinder import FixedPointFinder
+from FixedPoints import FixedPoints
+from plot_utils import plot_fps
+import pdb
+import numpy as np
 import tensorflow as tf
 class GridSearch(object):
     def __init__(self, model_class, hyperparameters):
@@ -91,8 +99,47 @@ class GridSearch(object):
         for param in params:
             print "\t{}: {} ".format(param, params[param])
 
+    def show_fixed_points(self, ff):
+        NOISE_SCALE = 0.5 # Standard deviation of noise added to initial states
+        N_INITS = 1024 # The number of initial states to provide
 
-    def run(self):
+        n_bits = ff.hps.data_hps['n_bits']
+        is_lstm = ff.hps.rnn_type == 'lstm'
+
+        '''Fixed point finder hyperparameters. See FixedPointFinder.py for detailed
+        descriptions of available hyperparameters.'''
+        fpf_hps = {}
+
+        # Setup the fixed point finder
+        fpf = FixedPointFinder(ff.rnn_cell,
+                               ff.session,
+                               **fpf_hps)
+
+        # Study the system in the absence of input pulses (e.g., all inputs are 0)
+        inputs = np.zeros([1,n_bits])
+        example_trials = ff.generate_SineWave_trials()
+
+        '''Draw random, noise corrupted samples of those state trajectories
+        to use as initial states for the fixed point optimizations.'''
+        example_predictions = ff.predict(example_trials,
+                                         do_predict_full_LSTM_state=is_lstm)
+
+        initial_states = fpf.sample_states(example_predictions['state'], n_inits=N_INITS, noise_scale=NOISE_SCALE)
+
+        # Run the fixed point finder
+        unique_fps, all_fps = fpf.find_fixed_points(initial_states, inputs)
+
+        # Visualize identified fixed points with overlaid RNN state trajectories
+        # All visualized in the 3D PCA space fit the the example RNN states.
+        plot_fps(unique_fps, example_predictions['state'],
+            plot_batch_idx=range(30),
+            plot_start_time=10)
+
+        print('Entering debug mode to allow interaction with objects and figures.')
+        pdb.set_trace()
+
+
+    def run(self, show_fixed_points=True):
         while self.has_next_param_set():
             params = self.get_next_param_set()
             self.print_model_params(params)
@@ -101,6 +148,9 @@ class GridSearch(object):
             print_status("Training model now")
             model.train()
             print_status("Done training model")
+            if show_fixed_points:
+                print_status("Showing Fixed Points")
+                self.show_fixed_points(model)
             print_status("Closing Session")
             tf.reset_default_graph()
             print_status("Session closed")
