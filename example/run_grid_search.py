ef094b8518a7bdabeb56ed89659ebe522946766f
Tim Wroge <tiw51@pitt.edu>
2020-03-27 11:48:16 -0400

    Fast grid search script

--- example/run_grid_search.py
+++ example/run_grid_search.py
@@ -1,7 +1,7 @@
 from GridSearch import GridSearch
 from SineWave import SineWave
 from Utils import print_status
-alr_hps = [ {'initial_rate': 0.0001, 'min_rate': 1e-5}, {'initial_rate': 0.0001, 'min_rate': 1e-4}]
+alr_hps = [ {'initial_rate': 0.0001, 'min_rate': 9.99e-5}, {'initial_rate': 0.0001, 'min_rate': 9.99e-5}]
 #alr_hps = [ {'initial_rate': 0.0001, 'min_rate': 1e-5}]
 hyper_params = {
     'rnn_type': [ 'vanilla'],
